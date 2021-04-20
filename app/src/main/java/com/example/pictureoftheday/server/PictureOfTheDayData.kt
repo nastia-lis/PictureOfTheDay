cd27ef54ee45eafcecdcf95943049ddeaba75c8c
Nastia <nastiusha_super@mail.ru>
2021-04-21 00:32:22 +0300

    hw1

---
+++ app/src/main/java/com/example/pictureoftheday/server/PictureOfTheDayData.kt
@@ -0,0 +1,7 @@
+package com.example.pictureoftheday.server
+
+sealed class PictureOfTheDayData {
+    data class Success(val serverResponseData: PODServerResponseData) : PictureOfTheDayData()
+    data class Error(val error: Throwable) : PictureOfTheDayData()
+    data class Loading(val progress: Int?) : PictureOfTheDayData()
+}
---
+++ app/src/main/java/com/example/pictureoftheday/viewmodel/PictureOfTheDayViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.pictureoftheday.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.pictureoftheday.BuildConfig
+import com.example.pictureoftheday.server.PODRetrofitImpl
+import com.example.pictureoftheday.server.PODServerResponseData
+import com.example.pictureoftheday.server.PictureOfTheDayData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class PictureOfTheDayViewModel(
+    private val liveData: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
+    private val retrofitImpl: PODRetrofitImpl = PODRetrofitImpl()
+) : ViewModel() {
+
+    fun getData(): LiveData<PictureOfTheDayData> {
+        sendServerRequest()
+        return liveData
+    }
+
+    private fun sendServerRequest() {
+        liveData.value = PictureOfTheDayData.Loading(null)
+        val apiKey: String = BuildConfig.NASA_API_KEY
+        if (apiKey.isBlank()) {
+            PictureOfTheDayData.Error(Throwable("Need api key"))
+        } else {
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(object : Callback<PODServerResponseData> {
+                override fun onResponse(
+                    call: Call<PODServerResponseData>,
+                    response: Response<PODServerResponseData>
+                ) {
+                    if (response.isSuccessful && response.body() != null) {
+                        liveData.value = PictureOfTheDayData.Success(response.body()!!)
+                    } else {
+                        val message = response.message()
+                        if (message.isNullOrEmpty()) {
+                            liveData.value = PictureOfTheDayData.Error(Throwable("Unknown error"))
+                        } else {
+                            liveData.value = PictureOfTheDayData.Error(Throwable(message))
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<PODServerResponseData>, t: Throwable) {
+                    liveData.value = PictureOfTheDayData.Error(Throwable(t))
+                }
+
+            })
+        }
+    }
+}
